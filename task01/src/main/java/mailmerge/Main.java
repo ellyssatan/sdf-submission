7a8b91dd9dd164ac4646d661b4e581d904216737
ellyssatan <ellyssa.tan1807@gmail.com>
2022-07-01 09:37:40 +0800

    initial commit

---
+++ task01/src/main/java/mailmerge/Main.java
@@ -0,0 +1,13 @@
+package mailmerge;
+
+import java.io.File;
+
+public class Main {
+    // java your.java.main <csv file> <template file>
+    public static void main(String[] args) {
+
+        String data = args[0];      //CSV file
+        String template = args[1];
+
+    }
+}
