3481612417994e356d2f6c72f46298ac861058cf
ellyssatan <ellyssa.tan1807@gmail.com>
2022-07-01 15:42:57 +0800

    completed task02

--- task01/src/main/java/mailmerge/Main.java
+++ task01/src/main/java/mailmerge/Main.java
@@ -9,17 +9,18 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main {
-    private static List<Object> text;
     private static String data;
     private static String template;
     private static String toReplace;
     private static ArrayList<String> wordsToReplace = new ArrayList<String>();
     static String[] dataText;
     private static List dataList = new ArrayList();
+    private static HashMap<String, Integer> map = new HashMap<String, Integer>();
 
 
     // java your.java.main <csv file> <template file>
@@ -33,7 +34,7 @@ public class Main {
         List<Object> tempText = fileFiller.read(template);
         List<Object> dataLines = fileFiller.read(data);
         // System.out.println(temptxt);
-        // System.out.println(datatxt);
+        // System.out.println(dataLines);
 
 
         
@@ -54,11 +55,27 @@ public class Main {
         for (int i = 0; i < dataLines.size(); i++) {
 
             dataText = ((String) dataLines.get(i)).split(",");     // separate individual fields
-            //System.out.println(Arrays.toString(dataText));
+            // System.out.println(Arrays.toString(dataText));
+            for (String w : wordsToReplace) {                       // loop through fields that need to replace
+                for (int k = 0; k < dataText.length; k++) {         // loop through CSV data
+                    if (dataText[k].equals(w)) {
+                        System.out.printf("%s at index %d\n", w, k);
+                        map.put(w, k);
+                        System.out.println(map);
+                        
+                    }
+                }
+            }
 
             Collections.addAll(dataList, dataText);
         }
-        System.out.println(dataList);
+
+        // List<Object> newFile = tempText.replaceAll(w);
+        // System.out.println(newFile);
+
+
+
+        // System.out.println(dataList);
 
         // System.out.println(wordsToReplace);
         // for (String w : wordsToReplace) {
@@ -77,8 +94,8 @@ public class Main {
         // }
 
         for (String w : wordsToReplace) {
-            for (int i = 0; i < dataLines.size(); i++) {
-                if (dataLines.get(i).equals(w)) {
+            for (int i = 0; i < dataText.length; i++) {
+                if (dataText[i].equals(w)) {
                     System.out.printf("%s at index %d\n", w, i);
                 }
             }
@@ -87,7 +104,7 @@ public class Main {
         for (Object d : dataText) {
             for (String s : wordsToReplace) {
                 switch (s) {
-                    
+
 
                 }
             }
--- task02/src/main/java/client/Client.java
+++ task02/src/main/java/client/Client.java
@@ -1,14 +1,21 @@
 package client;
 
 import java.io.Console;
+import java.io.DataInput;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class Client {
+
     static String requestID;
     static float total;
     static int count;
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
 
         int port = 80;
         String host = "68.183.239.26";
@@ -16,50 +23,53 @@ public class Client {
 
         Socket sock = new Socket(host, port);
 
-        NetworkIO netIO = new NetworkIO(sock);
-        // NetworkIO2 netIO = new NetworkIO2(sock);
-
-
-		// to get inputs	
-        Console cons = System.console();
-        String request = "";
-        String response = "";
-
+        OutputStream os = sock.getOutputStream();
+        InputStream is = sock.getInputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(os);
+        ObjectInputStream ois = new ObjectInputStream(is);
 
         while (true) {
-			// better to put inside while loop to prevent repeats
-            
-            request = netIO.read();
-            System.out.printf("[SERVER REQ] ", request);
+            String request = ois.readUTF();
+            // System.out.println(request);
 
             requestID = request.split(" ")[0];
             String[] intList = request.split(" ")[1].split(",");
 
-            netIO.write(requestID);
+            oos.writeUTF(requestID);
             System.out.printf("[CLIENT REPLY] requestID:", requestID);
 
-            String name  = cons.readLine("Full name as per NRIC: ");
-            netIO.write(name);
+            String name  = "Tan Jia Yi Ellyssa";
+            oos.writeUTF(name);
             System.out.printf("[CLIENT REPLY] Name:", name);
 
-            String email  = cons.readLine("Email: ");
-            netIO.write(email);
+            String email  = "ellyssa.tan1807@gmail.com";
+            oos.writeUTF(email);
             System.out.printf("[CLIENT REPLY] Email:", email);
-
 
             for (String i : intList) {
                 int num = Integer.parseInt(i);
-                num+=total;
+                total += num;
                 count++;
             }
             float average = total/count;
-            netIO.writeFloat(average);            
+            oos.writeFloat(average);            
             System.out.printf("[CLIENT REPLY] Average:", average);
 
-            netIO.close();
+            boolean result = ois.readBoolean();
+
+            if (result == true) {
+                System.out.println("SUCCESS");
+            } else {
+                System.out.println("FAILED");
+                String error = ois.readUTF();
+                System.out.println(error);
+            }
+
+            ois.close();
+            oos.close();
             sock.close();
-
+            
         }
-
+        
     }
 }
